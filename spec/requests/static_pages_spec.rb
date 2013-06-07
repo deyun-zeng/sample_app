a4ac1f3b9960d3bf2f5cd28f952a71dfb6123295
yumiweike <deyun.zeng@gmail.com>
2013-06-07 22:43:54 +0800

    Add a full title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,17 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+
+    it "should have the base title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
+    end
+    
   end
 
   describe "Help page" do
@@ -33,10 +44,10 @@ describe "Static pages" do
 
   describe "Dynamic Page Title" do
 
-    it "should have the right title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-    end
+    # it "should have the right title 'Home'" do
+    #   visit '/static_pages/home'
+    #   expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    # end
 
      it "should have the title 'Help'" do
       visit '/static_pages/help'
